8e89c3c859210296c665a99eae7d7117c4aff200
João <joao.graboski@jackexperts.com>
2025-03-24 16:40:01 -0300

    add doc & resolução t4

--- Codificador/CodificadorFactoryClass.java
+++ Codificador/CodificadorFactoryClass.java
@@ -1,4 +1,17 @@
 public class CodificadorFactoryClass implements CodificadorFactory {
+
+    private static CodificadorFactoryClass instance;
+
+    private CodificadorFactoryClass() {
+    }
+
+    public static CodificadorFactoryClass getInstance() {
+        if (instance == null) {
+            instance = new CodificadorFactoryClass();
+        }
+        return instance;
+    }
+
     @Override
     public Codificador getCodificador(int nivelSeguranca) {
         if (nivelSeguranca <= 1) {
